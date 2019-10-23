faa50414fd90f6547c4e4e519b12cea899438f3b
Tymothy J Hanks <tymjhanks@gmail.com>
2019-10-23 04:37:01 -0400

    was able to write to our indeed database

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ app.get('/scrape', async (req,res,next)=> {
     console.log('scrapping!')
     const [allIndeedDataPromised, allMonsterDataPromised] = await Promise.all([ getIndeedData(), getMonsterData() ]); //setting two variable at a time within an array and waiting till both functions are done with their Promise - passing in paramaters to make the functions dynamic based on user input"
     console.log(allIndeedDataPromised, allMonsterDataPromised) //console.loging the data
-    res.json({ allIndeedDataPromised, allMonsterDataPromised})
+    db.get('indeed').push({ //pushing our indeed scrapped data into our lowdb data base
+        datePosted: Date.now(), //adding a time stamp of when each job was posted
+        indeedJobs: allIndeedDataPromised //adding another key value pair which includes our data
+    }).write() //this writes everything inside this object into our data base
+
+    res.json({ allIndeedDataPromised, allMonsterDataPromised}) //turning the data into JSON so it can be displayed
 });
 
 
