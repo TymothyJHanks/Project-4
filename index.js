e863b5df2a728343dcc523c4814959066e28795e
Tymothy J Hanks <tymjhanks@gmail.com>
2019-10-22 23:23:05 -0400

    was able to make two smaller functions to pass in the data to the index.js 'go' function easier

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-import {getHTML, getJobListings, getJobListingsMonster} from './lib/scraper'; //importing our getHtml && getJobListings functions from scrapper.js
+import {getHTML, getIndeedData, getMonsterData} from './lib/scraper'; //importing our getHtml && getJobListings functions from scrapper.js
 
 
 async function go(jobTitle = 'software', jobCity = 'washington', jobState = 'dc'){ //this is creating a function with paramaters so that we can get user input to make these links dynamic based on what the user types in
-const indeedPromise = getHTML(`https://www.indeed.com/q-${jobTitle}-l-${jobCity},-${jobState}-jobs.html`); //this grabs the html link using axios being imported from our getHTML function and saves it in a variable
-const monsterPromise = getHTML(`https://www.monster.com/jobs/search/?q=${jobTitle}&where=${jobCity}__2C-${jobState}&intcid=skr_navigation_nhpso_searchMain&stpage=1&page=5`); //this grabs the html link using axios being imported from our getHTML function and saves it in a variable
-const [indeedHTML, monsterHTML] = await Promise.all([indeedPromise, monsterPromise]); //this is setting two new variables and awaiting the data being returned from each link at once then saving them both in their own separate variables
-const allIndeedData = await getJobListings(indeedHTML); //after the data comes back from the links, this is setting all the data into a variable and passing it into our function being imported
-const allMonsterData = await getJobListingsMonster(monsterHTML) //after the data comes back from the links, this is setting all the data into a variable and passing it into our function being imported
-console.log(`ALL INDEED DATA HERE: ${allIndeedData} ALL MONSTER DATA HERE: ${allMonsterData}`)
+const [allIndeedDataPromised, allMonsterDataPromised] = await Promise.all([ getIndeedData(jobTitle, jobCity, jobState), getMonsterData(jobTitle, jobCity, jobState) ]); //setting two variable at a time within an array and waiting till both functions are done with their Promise - passing in paramaters to make the functions dynamic based on user input"
+console.log(allIndeedDataPromised, allMonsterDataPromised) //console.loging the data
 }
 
 go();
