681e48db8a1adca81f211b92fabe79efdc706a93
Tymothy J Hanks <tymjhanks@gmail.com>
2019-10-22 21:41:10 -0400

    got my monster data html page to show up using axios and made it dynamic by adding in parameters that changes the url

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import {getHTML, getJobListings} from './lib/scraper'; //importing our getHtml && getJobListings functions from scrapper.js
+import {getHTML, getJobListings, getJobListingsMonster} from './lib/scraper'; //importing our getHtml && getJobListings functions from scrapper.js
 
 
 async function go(){
-    getJobListings( await getHTML('https://www.indeed.com/q-software-l-washington,-DC-jobs.html'));
+    // const html = await getHTML('https://www.indeed.com/q-software-l-washington,-DC-jobs.html');
+    // const jobT = await getJobListings(html);
+    // console.log(`these are your job titles: ${jobT}`)
+    const monsterData = await getJobListingsMonster('driver','baltimore','md')
 }
 
 go();
