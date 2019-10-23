c82f99c8bc886d0b3ac6c96db15d303feaf94387
Tymothy J Hanks <tymjhanks@gmail.com>
2019-10-22 22:29:17 -0400

    got the monster job data scraping to work and grabbed multiple data points from that

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ async function go(){
     // const html = await getHTML('https://www.indeed.com/q-software-l-washington,-DC-jobs.html');
     // const jobT = await getJobListings(html);
     // console.log(`these are your job titles: ${jobT}`)
-    const monsterData = await getJobListingsMonster('driver','baltimore','md')
+
+    const html = await getHTML('https://www.monster.com/jobs/search/?q=software&where=washington__2C-dc&intcid=skr_navigation_nhpso_searchMain');
+    const jobTM = await getJobListingsMonster(html);
 }
 
 go();
