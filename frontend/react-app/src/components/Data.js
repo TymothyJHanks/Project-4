2227a4311d66d3a680f79e9960f7a1694708c0a9
Tymothy J Hanks <tymjhanks@gmail.com>
2019-10-23 15:27:10 -0400

    added notes to everything

--- frontend/react-app/src/components/Data.js
+++ frontend/react-app/src/components/Data.js
@@ -1,12 +1,12 @@
 import React from 'react'; //importing react
-import {useContext} from 'react' //importing useContext to be able to use content in other components 
+import {useContext} from 'react' //importing useContext to be able to use data in other components 
 import {ScrapeContext} from '../components/ScrapeContext'; //importing our scrape context from scrapecontext.js - the data in is actually comming from the props passed down from the page.js file from the scrapeContext component in there 
 
 
 function Data(){
 
     //Functions
-    const scrapeData = useContext(ScrapeContext)
+    const scrapeData = useContext(ScrapeContext) //This is a reusable context hook that allows us to pull data from other components specically the ScrapeProvider in Page.js
 
     console.log(scrapeData)
 
--- frontend/react-app/src/components/ScrapeContext.js
+++ frontend/react-app/src/components/ScrapeContext.js
@@ -1,5 +1,5 @@
 import React from 'react' //importing react
 
-export const ScrapeContext = React.createContext();
+export const ScrapeContext = React.createContext(); //this allows us to put our data at a higher level to be able to be consumed by lower level components
 
-export const ScrapeProvider = ScrapeContext.Provider;
+export const ScrapeProvider = ScrapeContext.Provider; //putting the createContent into a variable/component to be able to pass data through as props to go to ourside component - data is being passed in our Page.js
