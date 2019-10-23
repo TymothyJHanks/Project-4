f9de549240d115773ffcf133ac6ca707cbd69441
Tymothy J Hanks <tymjhanks@gmail.com>
2019-10-23 17:14:55 -0400

    was able to get the data to display on the page from my backend

--- frontend/react-app/src/components/Data.js
+++ frontend/react-app/src/components/Data.js
@@ -1,7 +1,7 @@
 import React from 'react'; //importing react
+// import {distanceInWords} from "date-fns"; 
 import {useContext} from 'react' //importing useContext to be able to use data in other components 
 import {ScrapeContext} from '../components/ScrapeContext'; //importing our scrape context from scrapecontext.js - the data in is actually comming from the props passed down from the page.js file from the scrapeContext component in there 
-
 
 function Data(){
 
@@ -15,7 +15,15 @@ function Data(){
         <div className="data">
             <h2>Your Data:</h2>
             {/* You are able to use scrapeData.hey because the data being passed down is a prop of an object with key value pairs -  within a compoent thats being represented by the  useContext method - shits wild*/}
-            {passedDownScrapesData.indeed.length}
+            <div className='ScrapedData'>
+                <ul>
+                {passedDownScrapesData.indeed.map(inCallBack => (
+                    <li key={inCallBack.datePosted}>
+                        {inCallBack.indeedJobs.jobDescription}
+                    </li>
+                ))}
+                </ul>
+            </div>
         </div>
     )
 }
