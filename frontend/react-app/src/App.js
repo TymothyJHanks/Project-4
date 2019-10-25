a9af89f98c6f993a5e4cb452a5a49da1820815d3
Tymothy J Hanks <tymjhanks@gmail.com>
2019-10-25 00:35:42 -0400

    made 2 individal comments to route to and be able to use hover effects for nav bar

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -5,14 +5,19 @@ import './App.css';
 //Outside Imports
 import Page from '../src/components/Page' //importing page which is being styled
 import Data from '../src/components/Data'
+import IndeedOnly from '../src/components/IndeedOnly'
+import MonsterOnly from '../src/components/MonsterOnly'
 
 
 
+//Make routes to different pages when things are clicked and make the hover effect stuff here too
 
 function App() {
   return (
     <Page>
-      <Data/>
+      {/* <Data/> */}
+      {/* <IndeedOnly/> */} 
+      {/* <MonsterOnly/> */}
     </Page>
   );
 }
---
+++ frontend/react-app/src/components/MonsterOnly.js
@@ -0,0 +1,97 @@
+import React from 'react'; //importing react
+// import {distanceInWords} from "date-fns"; 
+import {useState} from 'react'; //importing use state so I'll be able to set state in here
+import {useContext} from 'react' //importing useContext to be able to use data in other components 
+import {ScrapeContext} from '../components/ScrapeContext'; //importing our scrape context from scrapecontext.js - the data in is actually comming from the props passed down from the page.js file from the scrapeContext component in there 
+// import '../css/Data.css' //importing data css
+
+function Data(){ //Main Component that everything is rendering from
+
+
+    //SETTINGS STATE AND VARIABLES
+    const log = console.log; //used this so I'm able to console log things easier
+
+    const [{displayLocationsMonster, displayCompanyMonster}, setDisplayM] = useState({ //setting my CSS display in state so I can use it to display content when needed
+        displayLocationsMonster: 'none',
+        displayCompanyMonster: 'none',
+    }); 
+    
+    const {passedDownScrapesData} = useContext(ScrapeContext) //This is a reusable context hook that allows us to pull data from other components specically the ScrapeProvider in Page.js - needed to destructor scrapes because i guess the data is one level deeper
+
+    log(passedDownScrapesData) //Logging te data in te console so I can know whats going on
+
+
+
+    //RENDERING AND RETURNING 
+    return( 
+        <div className="scrapeData">
+            {/* You are able to use scrapeData.hey because the data being passed down is a prop of an object with key value pairs -  within a compoent thats being represented by the  useContext method - shits wild*/}  
+                <div className="Monster">
+                    <h1>Your Monster Data</h1>
+                    <ul>
+                    {/* This is a function that loops over all the data - gives it a callback - and then displays the data that is being passed through from an array - also logs a key for the loop that is mandatory */}
+                    {passedDownScrapesData.monster.map((inCallBack, i) => (
+                        // This is logging a Key value for the loop
+                        <li key={i}>
+
+                        {/* This function below grabs diferent titles from the different arrays being posted */}
+                        <h2>Title: {inCallBack.monsterJobs.jobTitleM[1,2,3,4,5]}</h2>
+
+                        <h3 style={{display: displayLocationsMonster}}> Location: {inCallBack.monsterJobs.jobLocationM[1,2,3,4,5]}</h3>
+
+                        {/* This displays my information on the screen  */}
+                        <h3 style={{display: displayCompanyMonster}}> Company: {inCallBack.monsterJobs.jobCompanyM}</h3>
+
+                        </li>
+                    ))}
+                    </ul>
+
+                {/* This Button sets my display above to block so it will render on the page when clicked */}
+                <div className='allBtnsMonster'>
+
+                    {/* In these locations button div -  since the state has multiple things, the displayPayIndeed is being set to block but you MUST also set all key value pairs in this state so I need to also set the location back to whatever the location is and not to 'block' or 'none'*/}
+                    <div className='locationBtns'>
+                <button className="eachBtn" onClick={ () => 
+                    setDisplayM ( 
+                        {
+                            displayCompanyMonster: displayCompanyMonster, //setting the display to "block" to display the information
+                            displayLocationsMonster: 'block' //since this is in the same set state - I need to set this to whatever the CURRENT value of this key is so I set value to ITSELF
+                        }
+                        )}> Get All Job Locations </button>
+
+                <button className="eachBtn" onClick={ () => 
+                    setDisplayM ( 
+                        {
+                            displayCompanyMonster: displayCompanyMonster, //setting the display to 'none' to hide the information
+                            displayLocationsMonster: 'none', //since this is in the same set state - I need to set this to whatever the CURRENT value of this key is so I set value to ITSELF
+                        }
+                    )}> Hide All Job Locations </button>
+                        </div>
+
+
+                    {/* In these Descriptions button div -  since the state has multiple things, the displayPayIndeed is being set to block but you MUST also set all key value pairs in this state so I need to also set the location back to whatever the location is and not to 'block' or 'none'*/}
+                    <div className='companyBtns'>
+                <button className="eachBtn" onClick={ () => 
+                    setDisplayM ( 
+                        {
+                            displayLocationsMonster: displayLocationsMonster, //setting the display to "block" to display the information
+                            displayCompanyMonster: 'block' //since this is in the same set state - I need to set this to whatever the CURRENT value of this key is so I set value to ITSELF
+                        }
+                        )}> Get All Job Company </button>
+
+                <button className="eachBtn" onClick={ () => 
+                    setDisplayM ( 
+                        {
+                            displayLocationsMonster: displayLocationsMonster, //setting the display to 'none' to hide the information
+                            displayCompanyMonster: 'none', //since this is in the same set state - I need to set this to whatever the CURRENT value of this key is so I set value to ITSELF
+                        }
+                    )}> Hide All Job Company </button>
+                        </div>
+                </div>
+                </div>
+        </div>
+    )
+}
+
+
+export default Data
