26ac511e96d20f7cfb9fce9e326a4537933fb9e7
Tymothy J Hanks <tymjhanks@gmail.com>
2019-10-25 04:34:15 -0400

    working on routes and displaying things when things are clicked

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { Route, Link, NavLink } from "react-router-dom";
+import styled from 'styled-components';
 import logo from "./logo.svg";
 import "./App.css";
+import {useState} from 'react'; //importing use state so I'll be able to set state in here
+
 
 //Outside Imports
 import Page from "../src/components/Page"; //importing page which is being styled
@@ -8,20 +12,35 @@ import Data from "../src/components/Data"; //this is both components data in one
 import IndeedOnly from "../src/components/IndeedOnly";
 import MonsterOnly from "../src/components/MonsterOnly";
 
+
 //Make routes to different pages when things are clicked and make the hover effect stuff here too
- 
 
 
 function App() {
+
+const [{displayLocationsIndeed}, setDisplay] = useState({ //setting my CSS display in state so I can use it to display content when needed
+  displayLocationsIndeed: 'block',
+}); 
+
   return (
     <Page>
-      <div className="appWrapperBody">
-        <div className="all">
+      
+      <div style={{display: 'none'}}>
+      <IndeedOnly></IndeedOnly>
+      </div>
 
+      <div style={{display: displayLocationsIndeed}}className="appWrapperBody">
+        <div  className="all">
 
-          <div className="left">
-            <div className="text">Indeed</div>
+        <Link onClick={()=> setDisplay({
+          displayLocationsIndeed: 'none'
+        })} to="/indeed">
+          <div  className="left">
+            <div className="text"> Indeed
+              </div>
           </div>
+        </Link>
+
 
           <div className="center">
             <div className="text">Monster and Indeed</div>
@@ -34,6 +53,7 @@ function App() {
           </div>
 
 
+          <Route path="/indeed" exact component={IndeedOnly} />
 
         </div>
       </div>
